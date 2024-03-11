a13f70a8aaa8e91becd8e79d3865b007ceada1df
artemiusV <artemius.visotski@gmail.com>
2024-03-11 16:20:06 +0300

    added pagination and per page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 import axios from "../../../axios";
 import User from "../../ui/User.js";
@@ -7,6 +7,10 @@ const Home = () => {
   const [query, setQuery] = useState("");
   //users Fetched from API
   const [users, setUsers] = useState([]);
+  //Page
+  const [page, setPage] = useState(1);
+  //Per page
+  const [limit, setLimit] = useState(10);
 
   const handleQueryInput = (e) => {
     const value = e.target.value;
@@ -14,9 +18,30 @@ const Home = () => {
     // console.log(e.target.value);
   };
 
+  const handlePrevPage = () => {
+    setPage((page) => {
+      if (page === 1) return page;
+      else return page - 1;
+    });
+  };
+
+  const handleNextPage = () => {
+    setPage((page) => page + 1);
+  };
+
+  const handlePageLimit = (e) => {
+    const value = e.target.value;
+    setLimit(parseInt(value));
+  };
+
   const fetchUsers = async () => {
     try {
-      const { data } = await axios.get("/search/users?q=" + query);
+      const { data } = await axios.get("/search/users?q=" + query, {
+        params: {
+          page,
+          per_page: limit,
+        },
+      });
       return data?.items;
     } catch (error) {
       return null;
@@ -33,6 +58,16 @@ const Home = () => {
     }
   };
 
+  useEffect(() => {
+    const displayUserOnChange = async () => {
+      if (query) {
+        const items = await fetchUsers();
+        setUsers(items);
+      }
+    };
+    displayUserOnChange();
+  }, [page, limit]);
+
   return (
     <div className="container">
       <div className="search-form">
@@ -43,6 +78,21 @@ const Home = () => {
         </form>
       </div>
       <div className="search-result">
+        <div className="more-options">
+          <label>
+            <small>Per Page</small>
+            <select onChange={handlePageLimit}>
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
+            </select>
+          </label>
+          <div className="pagination">
+            <button onClick={handlePrevPage}>{page}</button>
+            <button onClick={handleNextPage}>{page + 1}</button>
+          </div>
+        </div>
         {users ? (
           users.map((user) => {
             return <User user={user} key={user.id} />;
